371638e235e4615da3bf62f9be1673c0e46d1cd8
Ricardo Subtil <ricasubtil@gmail.com>
2023-02-09 23:19:33 +0000

    Overhaul to wrap around UI behavior

--- scenes/config/AboutSettings.gd
+++ scenes/config/AboutSettings.gd
@@ -6,12 +6,17 @@ const ISSUES_URL := "https://github.com/retrohub-org/retrohub/issues"
 onready var n_version := $"%Version"
 onready var n_engine_version := $"%EngineVersion"
 
+onready var n_open_website_button = $"%OpenWebsiteButton"
+
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	n_version.text = n_version.text % RetroHub.version_str
 	n_engine_version.text = n_engine_version.text % Engine.get_version_info()["string"]
 
+func grab_focus():
+	n_open_website_button.grab_focus()
 
 func _on_RichTextLabel_meta_clicked(meta):
 	if "http" in meta:
