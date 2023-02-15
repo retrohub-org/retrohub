bea2e6cb36098ac6bbc94c73359d8a218a0e9078
Ricardo Subtil <ricasubtil@gmail.com>
2023-02-15 15:01:39 +0000

    Fix SpinBox focus issues

--- source/utils/ControllerHandler.gd
+++ source/utils/ControllerHandler.gd
@@ -2,13 +2,21 @@ extends Control
 
 var prev_focus : Control = null
 
+var hyper_focused_control : Control = null
+
 func _init():
 	ControllerIcons.connect("input_type_changed", self, "_on_input_type_changed")
+
+func _ready():
+	yield(get_tree(), "idle_frame")
+	raise()
 
 func _on_input_type_changed(input_type):
 	set_process(input_type == ControllerIcons.InputType.CONTROLLER)
 
 func _input(event):
+	if hyper_focused_control:
+		get_tree().set_input_as_handled()
 	if event is InputEventJoypadButton:
 		_input_button(event)
 	elif event is InputEventJoypadMotion:
@@ -16,8 +24,17 @@ func _input(event):
 
 func _input_button(event: InputEventJoypadButton):
 	var control := get_focus_owner()
-	if event.is_action_pressed("rh_accept"):
-		if control is LineEdit or control is TextEdit:
+	if hyper_focused_control:
+		_input_hyper_focused(event)
+	if RetroHubUI.is_virtual_keyboard_visible():
+		return
+	if event.is_action_released("rh_accept"):
+		if (control is LineEdit and control.editable) or (control is TextEdit and not control.readonly):
+			if control.get_parent() and control.get_parent() is SpinBox:
+				hyper_focused_control = control.get_parent()
+				control.theme_type_variation = "HyperFocused"
+				control.caret_position = control.text.length()
+				return
 			if prev_focus != control:
 				if control is LineEdit:
 					control.caret_position = control.text.length()
@@ -26,6 +43,26 @@ func _input_button(event: InputEventJoypadButton):
 					control.cursor_set_column(control.text.length())
 			prev_focus = control
 			RetroHubUI.show_virtual_keyboard()
+	if event.is_action_pressed("rh_back"):
+		if hyper_focused_control:
+			if hyper_focused_control is SpinBox:
+				hyper_focused_control.get_line_edit().theme_type_variation = ""
+			else:
+				hyper_focused_control.theme_type_variation = ""
+			hyper_focused_control = null
 
 func _input_motion(event: InputEventJoypadMotion):
-	pass
+	if hyper_focused_control:
+		_input_hyper_focused(event)
+		
+func _input_hyper_focused(event):
+	match hyper_focused_control.get_class():
+		"SpinBox":
+			_input_motion_spinbox(event)
+
+func _input_motion_spinbox(event):
+	var spin_box = hyper_focused_control as SpinBox
+	if event.is_action_pressed("rh_up"):
+		spin_box.value += 1
+	if event.is_action_pressed("rh_down"):
+		spin_box.value -= 1
---
+++ source/utils/SpinBoxHandler.gd
@@ -0,0 +1,80 @@
+extends Control
+class_name SpinBoxHandler
+
+## Hack for tackling focus issues with spinboxes
+## Must be added as a child component of a spin box, aka
+## get_parent() returns the spin box
+
+enum Dir {
+	TOP,
+	BOTTOM,
+	LEFT,
+	RIGHT,
+	NEXT,
+	PREVIOUS
+}
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	# This _ready is called before parent, we need to wait a frame for parent to initialize
+	yield(get_tree(), "idle_frame")
+	var spin_box : SpinBox = get_parent()
+	if not spin_box:
+		return
+	spin_box.focus_mode = FOCUS_ALL
+	spin_box.connect("focus_entered", self, "_on_focus_entered")
+	for dir in [
+		Dir.TOP,
+		Dir.BOTTOM,
+		Dir.LEFT,
+		Dir.RIGHT,
+		Dir.NEXT,
+		Dir.PREVIOUS
+	]:
+		handle_neighbors(dir)
+
+
+func handle_neighbors(dir: int):
+	var neighbor : NodePath
+	match dir:
+		Dir.TOP:
+			neighbor = get_parent().focus_neighbour_top
+		Dir.BOTTOM:
+			neighbor = get_parent().focus_neighbour_bottom
+		Dir.LEFT:
+			neighbor = get_parent().focus_neighbour_left
+		Dir.RIGHT:
+			neighbor = get_parent().focus_neighbour_right
+		Dir.NEXT:
+			neighbor = get_parent().focus_next
+		Dir.PREVIOUS:
+			neighbor = get_parent().focus_previous
+
+	if not neighbor:
+		return
+
+	var neighbor_obj = get_node("../" + neighbor)
+
+	var raw_path := ".."
+	for i in range(neighbor.get_name_count()):
+		raw_path += "/" + neighbor.get_name(i)
+	if neighbor_obj is SpinBox:
+		raw_path += "/" + neighbor_obj.get_line_edit().get_name()
+	print(raw_path)
+
+	match dir:
+		Dir.TOP:
+			get_parent().get_line_edit().focus_neighbour_top = NodePath(raw_path)
+		Dir.BOTTOM:
+			get_parent().get_line_edit().focus_neighbour_bottom = NodePath(raw_path)
+		Dir.LEFT:
+			get_parent().get_line_edit().focus_neighbour_left = NodePath(raw_path)
+		Dir.RIGHT:
+			get_parent().get_line_edit().focus_neighbour_right = NodePath(raw_path)
+		Dir.NEXT:
+			get_parent().get_line_edit().focus_next = NodePath(raw_path)
+		Dir.PREVIOUS:
+			get_parent().get_line_edit().focus_previous = NodePath(raw_path)
+
+func _on_focus_entered():
+	get_parent().get_line_edit().grab_focus()
