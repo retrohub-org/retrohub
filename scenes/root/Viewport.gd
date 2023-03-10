fe99449752c238dfb823bdeaea35df4e4fbfa57c
Ricardo Subtil <ricasubtil@gmail.com>
2023-03-10 18:09:46 +0000

    Fix error regarding null child

--- scenes/root/Viewport.gd
+++ scenes/root/Viewport.gd
@@ -1,11 +1,11 @@
 extends Viewport
 
-onready var n_theme_node := $NoTheme
+onready var n_theme_node : Node = $NoTheme
 
 func set_theme(node: Node):
-	remove_child(n_theme_node)
 	RetroHubMedia._clear_media_cache()
 	if is_instance_valid(n_theme_node):
+		remove_child(n_theme_node)
 		n_theme_node.queue_free()
 	n_theme_node = node
 	add_child(n_theme_node)
