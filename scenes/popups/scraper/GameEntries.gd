952bc9b628c20a149691c4d2cbd5ee934d80e0bf
Ricardo Subtil <ricasubtil@gmail.com>
2023-03-19 22:40:20 +0000

    Fix child focus since accessibility addon

--- scenes/popups/scraper/GameEntries.gd
+++ scenes/popups/scraper/GameEntries.gd
@@ -4,7 +4,7 @@ var last_entry : RetroHubScraperGameEntry = null
 var focused : bool
 
 func grab_focus():
-	get_child(0).grab_focus()
+	get_child(1).grab_focus()
 	_on_GameEntries_focus_entered()
 
 func _on_game_entry_selected(entry: RetroHubScraperGameEntry, by_app: bool):
