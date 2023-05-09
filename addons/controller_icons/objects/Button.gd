d7ce9e7245f9bfb176bf578a6ebf35f2cacf40ea
Ricardo Subtil <ricasubtil@gmail.com>
2023-05-09 13:29:25 +0100

    Update ControllerIcons to 4.x version

--- addons/controller_icons/objects/Button.gd
+++ addons/controller_icons/objects/Button.gd
@@ -2,37 +2,36 @@
 extends Button
 class_name ControllerButton
 
-@export var path: String : String = "": set = set_path
-@export var show_only : int = 0: set = set_show_only
-@export var force_type : int = 0: set = set_force_type
+@export var path : String = "":
+	set(_path):
+		path = _path
+		if is_inside_tree():
+			if force_type > 0:
+				icon = ControllerIcons.parse_path(path, force_type - 1)
+			else:
+				icon = ControllerIcons.parse_path(path)
+
+@export_enum("Both", "Keyboard/Mouse", "Controller") var show_only : int = 0:
+	set(_show_only):
+		show_only = _show_only
+		_on_input_type_changed(ControllerIcons._last_input_type)
+
+@export_enum("None", "Keyboard/Mouse", "Controller") var force_type : int = 0:
+	set(_force_type):
+		force_type = _force_type
+		_on_input_type_changed(ControllerIcons._last_input_type)
 
 func _ready():
-	ControllerIcons.connect("input_type_changed", Callable(self, "_on_input_type_changed"))
-	set_path(path)
+	ControllerIcons.input_type_changed.connect(_on_input_type_changed)
+	self.path = path
 
 func _on_input_type_changed(input_type):
 	if show_only == 0 or \
 		(show_only == 1 and input_type == ControllerIcons.InputType.KEYBOARD_MOUSE) or \
 		(show_only == 2 and input_type == ControllerIcons.InputType.CONTROLLER):
-		set_path(path)
+		self.path = path
 	else:
 		icon = null
-
-func set_path(_path: String):
-	path = _path
-	if is_inside_tree():
-		if force_type > 0:
-			icon = ControllerIcons.parse_path(path, force_type - 1)
-		else:
-			icon = ControllerIcons.parse_path(path)
-
-func set_show_only(_show_only: int):
-	show_only = _show_only
-	_on_input_type_changed(ControllerIcons._last_input_type)
-
-func set_force_type(_force_type: int):
-	force_type = _force_type
-	_on_input_type_changed(ControllerIcons._last_input_type)
 
 func get_tts_string() -> String:
 	if force_type:
